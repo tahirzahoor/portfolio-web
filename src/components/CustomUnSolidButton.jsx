89b7d7cd550bea5c3e4ea41ec1d5ece433759a49
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-07-07 19:54:23 +0500

    deploy

--- src/components/CustomUnSolidButton.jsx
+++ src/components/CustomUnSolidButton.jsx
@@ -1,19 +1,37 @@
-import { Button } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
 import { IoIosArrowForward } from "react-icons/io";
 
-export default function CustomUnSolidButton() {
+// export default function CustomUnSolidButton() {
+//   return (
+//     <Button
+//       fontSize={"xl"}
+//       fontWeight={"normal"}
+//       color={"primary.200"}
+//       border={"1px"}
+//       p={4}
+//       display={"flex"}
+//       variant={"unstyled"}
+//       rightIcon={<IoIosArrowForward />}
+//     >
+//       View More
+//     </Button>
+//   );
+// }
+import React from "react";
+import "../style/unSolidButton.css";
+import { Button, Text } from "@chakra-ui/react";
+const CustomUnSolidButton = () => {
   return (
-    <Button
-      fontSize={"xl"}
-      fontWeight={"normal"}
-      color={"primary.200"}
-      border={"1px"}
-      p={4}
-      display={"flex"}
-      variant={"unstyled"}
-      rightIcon={<IoIosArrowForward />}
-    >
-      View More
-    </Button>
+    <ul>
+      <li>
+       View More 
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </li>
+    </ul>
   );
-}
+};
+
+export default CustomUnSolidButton;
